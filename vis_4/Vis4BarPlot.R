39f00e261a5dd234b72ab7bd260d492901d524d7
Clara Chun <Clara@calvisitor-10-105-176-126.calvisitor.1918.berkeley.edu>
2016-08-03 12:21:24 -0700

    vis4 update

--- vis_4/Vis4BarPlot.R
+++ vis_4/Vis4BarPlot.R
@@ -6,57 +6,41 @@ library(stringr)
 library(scales)
 library(ggthemes)
 
-#room.type
-citydf = read_csv("updated_cleaned_four_cities.csv") %>% 
-  mutate(city = factor(City, levels = c("la", "ch", "sf", "ny"), 
-                       labels = c("Los Angeles", "Chicago", "San Francisco", "New York"))) %>% 
-  filter(Room.Type %in% c("Shared room","Private room","Entire home/apt")) %>%
-  group_by(City, Room.Type) %>% 
-  mutate(dailyavg = mean(Price))
+
 
 #property.type
 citydf_1 = read_csv("updated_cleaned_four_cities.csv") %>% 
+  #Relabel cities to full names
   mutate(city = factor(City, levels = c("la", "ch", "sf", "ny"), 
-                       labels = c("Los Angeles", "Chicago", "San Francisco", "New York"))) %>% 
-  filter(Room.Type %in% c("Shared room","Private room","Entire home/apt")) %>%
+                       labels = c("Los Angeles", 
+                                  "Chicago", 
+                                  "San Francisco", 
+                                  "New York"))) %>%
   group_by(City, Property.Type) %>% 
-  mutate(dailyavg = mean(Price))
+  mutate(dailyavg = mean(Price)) %>% 
+  #filter property type so that it only contains the common properties among
+  #all four cities
+  filter(Property.Type %in% c("Townhouse", 
+                              "Loft", 
+                              "House", 
+                              "Condominium", 
+                              "Bed & Breakfast", 
+                              "Apartment"))
 
-
-#ggplot for room type
-ggplot(citydf)+
-  geom_bar(aes(x = Room.Type, 
-               y = dailyavg, 
-               fill = Room.Type), 
-           color = "black",
-           stat="identity", 
-           position = position_dodge(), 
-           width = 0.2) + 
-  coord_flip() +
-  scale_y_continuous(labels = dollar_format(prefix = "$"),
-                     limits = c(0,300),
-                     breaks = seq(0,300,100)) +
-  facet_grid(~city) +
-  labs(title = "Average Daily Price of Airbnb by Room Type",
-       x = "Type of Room",
-       y = "Price") +
-  scale_fill_discrete(name = "Type of Room") +
-  theme_wsj()
 
 #ggplot for property type
 ggplot(citydf_1)+
   geom_bar(aes(x = Property.Type, 
                y = dailyavg, 
-               fill = Property.Type), 
+               fill = city), 
            color = "black",
            stat="identity", 
            position = position_dodge(), 
-           width = 0.2) + 
+           width = 0.8) + 
   coord_flip() +
   scale_y_continuous(labels = dollar_format(prefix = "$"),
                      limits = c(0,300),
                      breaks = seq(0,300,100)) +
-  facet_grid(~city) +
   labs(title = "Average Daily Price of Airbnb by Property Type",
        x = "Type of Room",
        y = "Price") +
