b49c6ed26486313fd3ab82da0ceaacc7775b2fc2
Clara Chun <Clara@calvisitor-10-105-177-127.calvisitor.1918.berkeley.edu>
2016-08-08 19:23:31 -0700

    vis8

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# finalproject-airbnb-team
+# Airbnb Final Project
+This project explores a cross-sectional comparison of Airbnb and the hotel industry in four US cities (Chicago, Los Angeles, New York, and San Francisco), as well as Airbnb pricing and accommodation patterns for each city. 
+
+
+## Folders
+* raw_data
+This folder contains the raw data files for each city that was scraped through the Airbnb site
+* clean_data
+This folder contains two folders: ***cities_cleaning*** and ***hotels_cleaning***. The ***cities_cleaning*** folder contains ***cleaning_script.Rmd*** that produces the ***updated_cleaned_four_cities.csv*** file, and inside the ***hotels_cleaning*** file, the ***final_hotels.R*** produces the ***cleaned_four_hotels.csv*** file. These csv’s are used to explore our analysis.  
+* eda
+* functions
+* presentation
+* paper
+
+
+## Packages
+* dplyr
+* ggplot2
+* ggthemes
+* readr
+* scales
+* stringr
+* tidyr
+
+
+## Instructions to Produce the Final Paper
+Open RStudio and knit the paper
---
+++ eda/vis_8/vis_8.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+library(scales)
+library(ggthemes)
+
+
+#property.type
+citydf_1 = read_csv("updated_cleaned_four_cities.csv") %>% 
+  group_by(City, Property.Type) %>% 
+  mutate(dailyavg = mean(Price)) 
+
+
+#ggplot for property type
+ggplot(citydf_1)+
+  geom_bar(aes(x = Property.Type, 
+               y = dailyavg, 
+               fill = Property.Type), 
+           color = "black",
+           stat="identity", 
+           position = position_dodge(), 
+           width = 0.8) + 
+  coord_flip() +
+  scale_y_continuous(labels = dollar_format(prefix = "$"),
+                     limits = c(0,300),
+                     breaks = seq(0,300,100)) +
+  labs(title = str_wrap("Figure 4: Average Daily Price of Airbnb by Property Type"),
+       x = "Type of Room",
+       y = "Price") +
+  scale_fill_discrete(name = "Type of Property") +
+  theme_gdocs() +
+  theme(
+    axis.title.x = element_text(size = 13, face = "bold"),
+    axis.title.y = element_text(size = 13, face = "bold"),
+    plot.title = element_text(size = 15, face = "bold")) +
+  facet_grid(~City)
+
+
+
