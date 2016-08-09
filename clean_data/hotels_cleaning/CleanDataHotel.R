8141de1c2752ebcde5d806807801cee801d9e1a8
jteng84 <judy.teng84@berkeley.edu>
2016-08-08 23:43:13 -0700

    Delete CleanDataHotel.R

--- clean_data/hotels_cleaning/CleanDataHotel.R
+++
@@ -1,8 +0,0 @@
-la_df = read_csv("cleaned_la_hotel.csv")
-ch_df = read_csv("cleaned_ch_hotel.csv")
-ny_df = read_csv("cleaned_ny_hotel.csv")
-sf_df = read_csv("cleaned_sf_hotel.csv")
-
-cleaned_four_hotels = rbind(la_df, ch_df, ny_df, sf_df)
-
-write_csv(cleaned_four_hotels, "cleaned_four_hotels.csv")
